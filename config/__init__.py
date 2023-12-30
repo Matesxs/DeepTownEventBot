63261bdbe65456db1be28a8c0e6b8c62d1dfcec9
Martin Douša <martindousa186@gmail.com>
2023-12-30 14:37:20 +0100

    added option to pass config file as first argument

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,10 @@
+import sys
+
 from features import dictionary_store
 from . import cooldowns
 from .strings import Strings
 
-config = dictionary_store.DictionaryStore.from_toml("config/config.toml", "config/config.template.toml")
+if len(sys.argv) > 1:
+  config = dictionary_store.DictionaryStore.from_toml(sys.argv[1], "config/config.toml", "config/config.template.toml")
+else:
+  config = dictionary_store.DictionaryStore.from_toml("config/config.toml", "config/config.template.toml")

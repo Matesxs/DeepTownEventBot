99d556a9f267d77ac5f81cdf9c33b40bbde77124
Martin Douša <martindousa186@gmail.com>
2024-01-18 01:06:28 +0100

    added graph with new entities per day

--- extensions/dt_statistics.py
+++ extensions/dt_statistics.py
@@ -28,29 +28,33 @@ async def send_stats(inter: disnake.CommandInteraction, user_data = None, guild_
     dataframe["active"] = dataframe["active"].round().astype(int)
     dataframe["all"] = dataframe["all"].round().astype(int)
     dataframe["active_percent"] = dataframe["active"] / dataframe["all"] * 100
+    dataframe["all_diff"] = dataframe["all"].diff().fillna(0).astype(int)
 
     # logger.info(f"\n{dataframe.head(50)}")
 
     dataframe.reset_index(inplace=True)
     return dataframe
 
-  def generate_graph_file(data, title: str, ylabel: str, all_label: str, active_percent: str, active_label: str, filename: str):
+  def generate_graph_file(data, title: str, ylabel: str, all_label: str, all_diff_label: str, active_percent: str, active_label: str, filename: str):
     if data is None: return None
 
     dataframe = process_data_to_dataframe(data)
 
     fig, ax1 = plt.subplots(sharex=True, sharey=False)
     fig.subplots_adjust(right=0.85)
-    fig.set_size_inches(10.5, 6.0)
+    fig.set_size_inches(11.0, 6.0)
     plt.title(title, fontsize=22)
     plt.ylabel(ylabel, fontsize=16)
 
     ax2 = ax1.twinx()
     ax2.spines.right.set_position(("axes", 1.12))
     ax3 = ax1.twinx()
+    ax3.spines.right.set_position(("axes", 1.27))
+    ax4 = ax1.twinx()
     ax1.grid(False)
     ax2.grid(False)
     ax3.grid(False)
+    ax4.grid(False)
 
     plot1 = ax1.bar(dataframe.date, dataframe["all"], width=0.8, color="#07e6ec", edgecolor="#07e6ec", label=all_label)
     ax1.set_xticks(ax1.get_xticks(), ax1.get_xticklabels(), rotation=20, ha='right')
@@ -75,7 +79,7 @@ async def send_stats(inter: disnake.CommandInteraction, user_data = None, guild_
     distance = max_val - min_val
     ax2.set_ylim(max(min_val - 0.1 * distance, 0), max_val + 0.1 * distance)
 
-    plot3, = ax3.plot(dataframe.date, dataframe.active, marker="o", color="#f851b7", label=active_label)
+    plot3, = ax3.plot(dataframe.date, dataframe.all_diff, marker="o", color="#00ff41", label=all_diff_label)
     mplcyberpunk.make_lines_glow(ax3)
     # mplcyberpunk.add_gradient_fill(ax3, 0.5)
     mplcyberpunk.add_underglow(ax3, alpha_underglow=0.2)
@@ -83,11 +87,23 @@ async def send_stats(inter: disnake.CommandInteraction, user_data = None, guild_
     ax3.yaxis.set_major_formatter(mticker.ScalarFormatter())
     ax3.tick_params(axis='y', colors=plot3.get_color())
     ax3.get_xaxis().set_visible(False)
-    min_val, max_val = dataframe["active"].values.min(), dataframe["active"].values.max()
+    min_val, max_val = dataframe["all_diff"].values.min(), dataframe["all_diff"].values.max()
     distance = max_val - min_val
     ax3.set_ylim(max(min_val - 0.1 * distance, 0), max_val + 0.1 * distance)
 
-    fig.legend(handles=[plot1, plot2, plot3], loc="upper left", fontsize=16, bbox_to_anchor=(0,0,1,1), bbox_transform=ax1.transAxes, frameon=True, facecolor="black", framealpha=0.40, edgecolor="black")
+    plot4, = ax4.plot(dataframe.date, dataframe.active, marker="o", color="#f851b7", label=active_label)
+    mplcyberpunk.make_lines_glow(ax4)
+    # mplcyberpunk.add_gradient_fill(ax4, 0.5)
+    mplcyberpunk.add_underglow(ax4, alpha_underglow=0.2)
+    ax4.yaxis.set_major_locator(mticker.MaxNLocator(nbins=10, integer=True))
+    ax4.yaxis.set_major_formatter(mticker.ScalarFormatter())
+    ax4.tick_params(axis='y', colors=plot4.get_color())
+    ax4.get_xaxis().set_visible(False)
+    min_val, max_val = dataframe["active"].values.min(), dataframe["active"].values.max()
+    distance = max_val - min_val
+    ax4.set_ylim(max(min_val - 0.1 * distance, 0), max_val + 0.1 * distance)
+
+    fig.legend(handles=[plot1, plot2, plot3, plot4], loc="upper left", fontsize=16, bbox_to_anchor=(0,0,1,1), bbox_transform=ax1.transAxes, frameon=True, facecolor="black", framealpha=0.5, edgecolor="black")
 
     fig.tight_layout()
 
@@ -103,10 +119,10 @@ async def send_stats(inter: disnake.CommandInteraction, user_data = None, guild_
   files = []
 
   if user_data is not None:
-    files.append(generate_graph_file(user_data, "User Statistics", "Users", "All Users", "Percentage of Active Users", "Active Users", "user_graph.png"))
+    files.append(generate_graph_file(user_data, "User Statistics", "Users", "All Users", "New Users", "Percentage of Active Users", "Active Users", "user_graph.png"))
 
   if guild_data is not None:
-    files.append(generate_graph_file(guild_data, "Guild Statistics", "Guilds", "All Guilds", "Percentage of Active Guilds", "Active Guilds", "guild_graph.png"))
+    files.append(generate_graph_file(guild_data, "Guild Statistics", "Guilds", "All Guilds", "New Guilds", "Percentage of Active Guilds", "Active Guilds", "guild_graph.png"))
 
   if files:
     for file in files:
